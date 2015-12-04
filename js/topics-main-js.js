016ea24a91a66a81e534eef2ad62407a1b235fb8
unknown <raghavgoel26@gmail.com>
2015-12-04 12:31:10 +0530

    category & topics page added

---
+++ js/topics-main-js.js
@@ -0,0 +1,46 @@
+(function(){
+
+  console.log("Hello");
+  $.getJSON( "data/topics-main-json.json", function( data ) {
+
+  console.log(data);
+
+
+  var l=data.length;
+  //console.log(l);
+
+
+    var cname=data[0]["Category Name"];
+    var x=$('.topic-category-name');
+    x.text(cname);
+    var i;
+
+     console.log("Hello");
+    for(j=0;j<5;++j)
+    {
+      k=j+1;
+      z=($('.collection-widget:nth-child('+ k +')'));
+      y=z.find($('.topic')).first();
+      for(i=0;i<5;++i)
+      {
+        console.log(i);
+        r=y.find('.topic-name');
+        r.text(data[j]["Category Topics"][i]["Topic Name"]);
+        r=y.find('.category-name');
+        r.text(data[j]["Category Topics"][i]["Topic Category"]);
+        r=y.find('.topic-icon')
+            .find('img');
+        console.log(r);
+        var src=data[j]["Category Topics"][i]["Topic Icon"];
+        console.log(src);
+        r.attr("src",src);
+        y=y.next();
+      }
+    //console.log(x);
+//  z=z.next();
+  //  console.log(z);
+  }
+
+  });
+
+})();
