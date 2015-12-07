025029a33fed974f1f28372cbb45f02bc44226a9
unknown <raghavgoel26@gmail.com>
2015-12-07 10:18:55 +0530

    Category Page made dynamic

---
+++ js/topic-play-js.js
@@ -0,0 +1,9 @@
+var a=$('.topic-user').height();
+console.log(a);
+
+var b=$('.topic-icon img').height(a);
+
+var a=$('.leaderboard span').height();
+console.log(a);
+
+var b=$('.leaderboard img').height(a);
