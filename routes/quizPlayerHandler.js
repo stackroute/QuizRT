b0b0990ba04e82a44c91d8f6474fc680ea89a3b1
akshay111meher <akshay_111meher@hotmail.com>
2015-12-18 16:11:53 +0530

    data changed

---
+++ routes/quizPlayerHandler.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var contents = JSON.parse(fs.readFileSync('public/data/quiz.json'));
+var contents1 = JSON.parse(fs.readFileSync('public/data/quizProperties.json'));
+
+/* GET home page. */
+router.post('/quizData', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.json(contents);
+});
+
+router.post('/quizProperties', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.json(contents1);
+});
+
+router.get('/submitresult', function(req, res, next) {
+  console.log("passed");
+  console.log(req.query);
+});
+
+router.get('/', function(req, res, next) {
+  res.render('quizPlayer')
+});
+
+module.exports = router;
